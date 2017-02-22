ebd896a07ffebc8563116aa4d0753d9443977f15
Jack Lu <jacklu@jacklu.me>
2017-02-22 23:23:02 +0800

    +specificity

--- cssx.js
+++ cssx.js
@@ -1,8 +1,29 @@
 function cssx(cssx) {
 	return function (element) {
-		for (var i in cssx) {
-			var rule = cssx[i];
-			element.children(rule.selector).data('cssx', rule.style);
+		var ruleelement =
+			cssx.map(function (rule) {
+				return {
+					rule: rule,
+					element: element.children(rule.selector)
+				};
+			});
+		var elementrule = {};
+		var id = 0;
+		for (var i in ruleelement)
+			ruleelement[i].element.each(function (j, element) {
+				if (!('$id' in element))
+					elementrule[element.$id = id++] = {
+						element: element,
+						rule: []
+					};
+				elementrule[element.$id].rule.push(ruleelement[i].rule);
+			});
+		for (var id in elementrule) {
+			var rule = elementrule[id].rule;
+			rule.sort(function (r, s) {
+				return SPECIFICITY.compare(r.selector, s.selector);
+			});
+			$(elementrule[id].element).data('cssx', rule[rule.length - 1].style);
 		}
 	};
 }
