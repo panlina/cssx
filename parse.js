c44ae2fffda15d27253ed9d53743c6ac5f865225
Jack Lu <jacklu@jacklu.me>
2017-02-23 17:16:49 +0800

    +parse

---
+++ parse.js
@@ -0,0 +1,36 @@
+cssx.parse = function (text) {
+	var sheet = [];
+	for (var i = 0; ;) {
+		parseSpace();
+		if (i >= text.length)
+			break;
+		var rule = {};
+		rule.selector = parseSelector();
+		rule.style = parseStyle();
+		sheet.push(rule);
+	}
+	return sheet;
+	function parseSelector() {
+		var i0 = i;
+		while (text[i] != '{') i++;
+		return text.substr(i0, i - i0);
+	}
+	function parseStyle() {
+		var i0 = i;
+		i++;
+		while (!isSpace(text[i]) || text[i + 1] != '}') i++;
+		i++, i++;
+		var style = text.substr(i0, i - i0);
+		return JSON.parse(style);
+	}
+	function parseSpace() {
+		for (; i < text.length; i++) {
+			var c = text[i];
+			if (!isSpace(c))
+				break;
+		}
+	}
+	function isSpace(c) {
+		return c == ' ' || c == '\t' || c == '\n';
+	}
+};
